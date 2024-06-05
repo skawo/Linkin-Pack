c91af50f374b19a15fda2f38f52342d148cfc2fd
skawo <43761362+skawo@users.noreply.github.com>
2024-06-05 16:35:08 +0200

    Update MainForm.cs

--- OoT Link Animation Editor/MainForm.cs
+++ OoT Link Animation Editor/MainForm.cs
@@ -576,7 +576,7 @@ namespace OoT_Link_Animation_Editor
 
         private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Link Animation Manager 1.04 for Ocarina of Time\nProgramming by: Skawo, 2022\nThanks to: SageofMirrors, Cloudmodding.");
+            MessageBox.Show("Link Animation Manager 1.05 for Ocarina of Time\nProgramming by: Skawo, 2022-2024\nThanks to: SageofMirrors, Cloudmodding.");
         }
 
         private void SaveAsToolStripMenuItem_Click(object sender, EventArgs e)
